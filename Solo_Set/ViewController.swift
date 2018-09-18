2eb72cfb9f556b4865c75d7c66afe97c48f6230d
Bishal Dahal <bishaldahal99@gmail.com>
2018-09-18 21:11:07 +1000

    Initial Commit

---
+++ Solo_Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Solo_Set
+//
+//  Created by Bishal Dahal on 18/9/18.
+//  Copyright © 2018 Bishal Dahal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
